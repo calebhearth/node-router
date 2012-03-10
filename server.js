2542b9471551984304a7c89a8be2b90ba0ca0961
Caleb Thompson <caleb@solsystemscompany.com>
2012-03-10 11:53:39 -0500

    Make useful logging happen

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require("http");
 
 http.createServer(function(request, response) {
-  console.log("Request received.");
+  console.log(request.method, request.url);
   response.writeHead(200, { "content-Type":  "text/plain" });
   response.write("Hello World");
   response.end();
