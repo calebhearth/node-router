a31a415b0c539de9efe847671df4951ee0585f59
Caleb Thompson <caleb@solsystemscompany.com>
2012-03-10 12:04:10 -0500

    Modularize the server, start it from index.js

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 var http = require("http");
 
-function onRequest(request, response) {
-  console.log(request.method, request.url);
-  response.writeHead(200, { "content-Type":  "text/plain" });
-  response.write("Hello World");
-  response.end();
+function start() {
+  function onRequest(request, response) {
+    console.log(request.method, request.url);
+    response.writeHead(200, { "content-Type":  "text/plain" });
+    response.write("Hello World");
+    response.end();
+  }
+
+  http.createServer(onRequest).listen(8888);
+
+  console.log("Server has started.");
 }
 
-http.createServer(onRequest).listen(8888);
+exports.start = start;
 
-console.log("Server has started.");
