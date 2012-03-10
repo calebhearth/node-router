a0855edbc2188e182714f411cf17c261838ac40d
Caleb Thompson <caleb@solsystemscompany.com>
2012-03-10 11:46:29 -0500

    Add logging for server start, on request received.

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var http = require("http");
 
 http.createServer(function(request, response) {
+  console.log("Request received.");
   response.writeHead(200, { "content-Type":  "text/plain" });
   response.write("Hello World");
   response.end();
 }).listen(8888);
+
+console.log("Server has started.");
